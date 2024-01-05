559272d6cd93d2f2d9e1755e6659bb177d538e70
Charles Frye <cfrye59@gmail.com>
2024-01-05 13:44:49 -0600

    closes #2, adds validation to signup route

--- src/routes/signup.js
+++ src/routes/signup.js
@@ -1,8 +1,9 @@
 import express from "express";
 import bcrypt from "bcryptjs";
+import expressAsyncHandler from "express-async-handler";
+import { body, validationResult } from "express-validator";
 
 import models from "../models/";
-import expressAsyncHandler from "express-async-handler";
 
 const router = express.Router();
 
@@ -12,14 +13,33 @@ router.get("/", (req, res) => {
 
 router.post(
   "/",
+  body("username")
+    .isLength({ min: 3 })
+    .withMessage("Username must be at least 3 characters long"),
+  body("password")
+    .isLength({ min: 5 })
+    .withMessage("Password must be at least 5 characters long"),
+  body("confirmPassword")
+    .custom((value, { req }) => {
+      return value === req.body.password;
+    })
+    .withMessage("Passwords must match"),
   expressAsyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.render("signup_form", {
+        title: "signup",
+        error: errors.array()[0].msg
+      });
+    }
+
     const existingUser = await models.User.findOne({
       username: req.body.username
     });
     if (existingUser) {
       res.render("signup_form", {
         title: "signup",
-        error: "Username already exists"
+        error: "Username taken"
       });
       return;
     }
