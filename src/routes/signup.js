0ee636649b264250b54e0e0ea7cafcb4daa1c426
Charles Frye <cfrye59@gmail.com>
2024-01-04 19:28:28 -0600

    prevent crash if user puts existing username into signup

--- src/routes/signup.js
+++ src/routes/signup.js
@@ -11,6 +11,16 @@ router.get("/", (req, res) => {
 
 router.post("/", async (req, res, next) => {
   try {
+    const existingUser = await models.User.findOne({
+      username: req.body.username
+    });
+    if (existingUser) {
+      res.render("signup_form", {
+        title: "signup",
+        error: "Username already exists"
+      });
+      return;
+    }
     bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
       if (err) {
         next(err);
