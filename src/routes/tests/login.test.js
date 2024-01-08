ac7046cd0b2284acf5dea2a5466be8355ed20e67
Charles Frye <cfrye59@gmail.com>
2024-01-07 21:11:23 -0800

    closes #14, adds more tests for login

--- src/routes/tests/login.test.js
+++ src/routes/tests/login.test.js
@@ -1,8 +1,11 @@
 import path from "path";
-import request from "supertest";
+import supertest from "supertest";
 import express from "express";
+import mongoose from "mongoose";
+
 import login from "../login";
 import addMiddlewares from "../../middleware";
+import { connectDb } from "../../models";
 
 const app = express();
 app.set("views", path.join(__dirname, "../../../views"));
@@ -18,5 +21,47 @@ app.use((error, _req, res, _next) => {
 });
 
 test("index route works ", (done) => {
-  request(app).get("/").expect("Content-Type", /html/).expect(200, done);
+  supertest(app).get("/").expect("Content-Type", /html/).expect(200, done);
+});
+
+describe("login integrity", () => {
+  let agent;
+
+  beforeAll(async () => {
+    await connectDb();
+    agent = supertest.agent(app);
+  });
+
+  afterAll(async () => {
+    await mongoose.disconnect();
+  });
+
+  test("login with bad credentials should fail and redirect to /login", async () => {
+    const response = await supertest(app).post("/").type("form").send({
+      username: "wu", // invalid username
+      password: "wrongpassword"
+    });
+    expect(response.statusCode).toBe(302);
+    expect(response.headers.location).toBe("/login");
+  });
+
+  test("login with good credentials should succeed and redirect to /", async () => {
+    const response = await supertest(app).post("/").type("form").send({
+      username: process.env.GOODUSER,
+      password: process.env.GOODPASS
+    });
+    expect(response.statusCode).toBe(302);
+    expect(response.headers.location).toBe("/");
+  });
+
+  test("logout after login should succeed and redirect to /", async () => {
+    await agent
+      .post("/")
+      .type("form")
+      .send({ username: "charles", password: "charles" })
+      .expect(302)
+      .expect("location", "/");
+
+    await agent.post("/out").expect(302).expect("location", "/");
+  });
 });
